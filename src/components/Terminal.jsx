933d217939725c79f96da27a57f5e3a729ac0884
Lorenzo <lorenzo.f@students.opit.com>
2025-11-12 19:55:11 +0100

    added fixed logic for text printing in terminal

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -4,19 +4,23 @@ function Terminal(props){
 
     const description = "Thank you for having visited my Portfolio website! Don't hesitate to contact me for any queston or proposal!";
     const [descript, setDescript] = React.useState("");
+    const [write, setWrite] = React.useState(true);
 
     function closeTerminalFunc(){
         props.closeTerminal()
     }
 
     function createAnimaiton(){
-        for(let i = 0; i < description.length; i++){
-            setTimeout(() => {setDescript(x => x + description.charAt(i))}, 50 * i);
+        if(write){
+            for(let i = 0; i < description.length; i++){
+                setTimeout(() => {setDescript(x => x + description.charAt(i))}, 50 * i);
+            }
+            setWrite(false);
         }
     }
 
     return(
-        <div className="terminal-container" onClick={createAnimaiton}>
+        <div className="terminal-container" onMouseOver={createAnimaiton}>
             <button onClick={closeTerminalFunc}>x</button>
             <p>lorenzofaccio@portfolio ~ /project  nano Thank_You.txt </p>
             <p>{descript}</p>
