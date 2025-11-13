5951ab309ddd6d1067a368f8b97fb40f7c24501e
Lorenzo <lorenzo.f@students.opit.com>
2025-11-13 14:50:44 +0100

    fixed homepage to alwas display personal info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,8 +23,8 @@ function App() {
     <div>
       <Header sendFunc={displayTerminal}/>
       {terminal && <Terminal closeTerminal={closeTerminal}/>}
-
       <Routes>
+        <Route path='/' element={<Home/>} />
         <Route path='/Home' element={<Home/>}></Route>
         <Route path='/Projects' element={<Projects/>}></Route>
         <Route path='/Contact' element={<Contact/>}></Route>
